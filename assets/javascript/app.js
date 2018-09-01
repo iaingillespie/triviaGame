a9e5f7fe56117d2c1a61d3d1ef132e1f1da74eea
Iain G <iaingill.law@gmail.com>
2018-09-01 17:07:26 -0400

    game logic complete, styleing and images - todo: incorrect if no selection

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,62 +3,238 @@ console.log("we are linked")
 // how to org qs and answers - array of objects.
 
 var qAndA = [{
-    question: "Question1",
-    answer: ["Answer1", "Answer2", "Answer3", "Answer4"],
-    correct: "Answer1"
+    question: "Where are we?",
+    answer: ["New York", "Auckland", "Bali", "Cape Town"],
+    correct: "Auckland",
+    image: "assets/images/Auckland.jpg"
 },
 
 {
-    question: "Question2",
-    answer: ["Answer1", "Answer2", "Answer3", "Answer4"],
-    correct: "Answer2"
+    question: "Where are we?",
+    answer: ["Kathmandu", "Ho Chi Minh City", "Chiang Mai", "Tokyo"],
+    correct: "Chiang Mai",
+    image: "assets/images/chaingmai.jpg"
 },
 
 {
-    question: "Question3",
-    answer: ["Answer1", "Answer2", "Answer3", "Answer4"],
-    correct: "Answer3"
+    question: "Where are we?",
+    answer: ["Marrakesh", "London", "Cairo", "Riyadh"],
+    correct: "Marrakesh",
+    image: "assets/images/Marrakesh.jpg"
+},
+
+{
+    question: "Where are we?",
+    answer: ["Mexico City", "Quito", "Austin", "Berlin"],
+    correct: "Quito",
+    image: "assets/images/_quito.jpg"
+},
+
+{
+    question: "Where are we?",
+    answer: ["Kingston", "Doha", "Lisbon", "Tel Aviv"],
+    correct: "Tel Aviv",
+    image: "assets/images/TelAviv.jpg"
+},
+
+{
+    question: "Where are we?",
+    answer: ["Shainghai", "Tokyo", "Philadelphia", "Paris"],
+    correct: "Tokyo",
+    image: "assets/images/tokyo.jpg"
 }
+
 ];
 
-console.log(qAndA);
-
-function gameStart (){
-    var currentQuestion =0;
-    $("#qh3").text(qAndA[currentQuestion].question)
-
-console.log(qAndA[currentQuestion].answer.length)
-    for(var i = 0; i < qAndA[currentQuestion].answer.length; i++){
-        console.log(i)
-        $("#answers").append("<input type ='radio' name ='Q-"+ currentQuestion + "' value ='"+ qAndA[currentQuestion].answer[i] + "''>" + qAndA[currentQuestion].answer[i] + "<br>" );
+var timeCount = 15;
+var intervalId;
+var gameStarted = false;
+var correct = 0;
+var incorrect = 0;
+var currentQuestion = 0;
 
 
+$("#start").on("click", gameStart);
 
-            // string 
-       // `<input type="radio" name="Q-${i}">`
+$("#reset").on("click", reset);
 
+function checkGameOver() {
+    if (currentQuestion > 5){
+    $("#answers").empty();
+    $("#answers").html("<h1>HE HE Game Over! You got "+correct+"/6 correct! Well Done !</h1> ");
+    clearInterval(intervalId);
+    $("#timer").empty();
+    $("#qh3").empty();
+    $("#imagecontainer").empty();
+    $("#imagecontainer").append('<img src="assets/images/worldgif.gif" alt="worldgif" height="400" width="400">');
+
+}
+}
+
+
+
+
+function gameStart() {
+
+    if (currentQuestion === qAndA.length) {
+        endGame();
+        alert("game over");
     }
 
-    //startTimer()
+    else if (gameStarted === false) {
+        gameStarted = true;
+
+        nextQuestion();
+    }
+
+};
+
+function reset () {
+    correct = 0;
+    incorrect = 0;
+    currentQuestion = 0;
+    $("#qh3").empty();
+    $("#answers").empty();
+    $("#answers").text("Pres Start to Begin");
+    $("#timer").empty();
+    $("#imagecontainer").empty();
+    clearInterval(intervalId);
+    timeCount = 15;
+    endGame();
 
 }
 
-gameStart();
 
 
-//$.each($(input[name='Q-0]:checked), function(){
-   // if($(this).val() ===qAndA[0].correctanswer){
-       //console.log("WIN")
-   //}
-//})
+// Timer Function
+
+function timer() {
+
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+};
+
+function decrement() {
+    timeCount--;
+    //  Show the number in the #show-number tag.
+    $("#timer").html("<h2>Time remaining:" + timeCount + "</h2>");
+
+    if (currentQuestion > qAndA.length){
+        endGame();
+    }
+    
+    else if (timeCount === 0) {
+        clearInterval(intervalId);
+        answerCheck();
+        currentQuestion++;
+        timeCount = 15;
+        nextQuestion();
+    };
+};
+
+function nextQuestion() {
+    checkGameOver();
+    $("#qh3").text(qAndA[currentQuestion].question)
+    $("#answers").empty();
+    for (var i = 0; i < qAndA[currentQuestion].answer.length; i++) {
+
+    $("#answers").append("<input type ='radio' name ='Q-" + currentQuestion + "' value ='" + qAndA[currentQuestion].answer[i] + "''>" + qAndA[currentQuestion].answer[i] + "<br>");
+
+    $("#imagecontainer").empty();
+    $("#imagecontainer").append("<img src='" + qAndA[currentQuestion].image +"' height='300' width='450' margin-right=auto margin-left=auto >");
 
 
-// initialise timer
-// append q1
 
-// funciton check answer
+    };
 
-// funciton check timer
+    timer();
+
+};
+
+function answerCorrect () {
+    correct++;
+    alert("correct");
+    console.log("Correct " + correct);
+    console.log("Incorrect " + incorrect);
+};
+
+function answerIncorrect () {
+    incorrect++;
+    alert("Incorrect");
+    console.log("Correct " + correct);
+    console.log("Incorrect " + incorrect);
+};
+
+
+function answerCheck() {
+
+    $.each($('input[name="Q-0"]:checked'), function () {
+        if ($(this).val() === qAndA[0].correct) {
+            answerCorrect();
+        }
+
+
+        else {
+            answerIncorrect();
+        }
+        
+        
+        });
+
+        $.each($('input[name="Q-1"]:checked'), function () {
+            if ($(this).val() === qAndA[1].correct) {
+                answerCorrect();
+            }
+            else {
+                answerIncorrect();
+            }
+        });
+
+        $.each($('input[name="Q-2"]:checked'), function () {
+            if ($(this).val() === qAndA[2].correct) {
+                answerCorrect();
+            }
+            else {
+                answerIncorrect();
+            }
+        });
+
+        $.each($('input[name="Q-3"]:checked'), function () {
+            if ($(this).val() === qAndA[3].correct) {
+                answerCorrect();
+            }
+            else {
+                answerIncorrect();
+            }
+        });
+
+        $.each($('input[name="Q-4"]:checked'), function () {
+            if ($(this).val() === qAndA[4].correct) {
+                answerCorrect();
+            }
+            else {
+                answerIncorrect();
+            }
+        });
+
+        $.each($('input[name="Q-5"]:checked'), function () {
+            if ($(this).val() === qAndA[5].correct) {
+                answerCorrect();
+            }
+            else {
+                answerIncorrect();
+            }
+        });
+
+    };
+
+    function endGame() {
+        gameStarted = false;
+    }
+
+
+
+
 
 
 
